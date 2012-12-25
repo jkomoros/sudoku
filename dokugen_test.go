73a120b21faf0a9438742e2d5592a3a1748bb877
Alex Komoroske <jkomoros@gmail.com>
2012-12-25 09:34:19 -0500

    Incredibly basic definition of Grid.

---
+++ dokugen_test.go
@@ -0,0 +1,9 @@
+package dokugen
+
+import (
+	"testing"
+)
+
+func TestBasic(t * testing.T) {
+	_ = Grid{data: "data"}
+}
---
+++ grid.go
@@ -0,0 +1,12 @@
+package dokugen
+
+const DIM = 9
+
+type Cell struct {
+	data string
+}
+
+type Grid struct {
+	data string
+	cells [DIM * DIM]Cell
+}
