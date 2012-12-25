ee38c48a0a366b881c1d2aafecc8de0ebd6a0546
Alex Komoroske <jkomoros@gmail.com>
2012-12-25 10:05:24 -0500

    Changed it so All is implemented in terms of Without for simpleCellList.

--- grid.go
+++ grid.go
@@ -29,23 +29,16 @@ type Grid struct {
 }
 
 func (self *simpleCellList) All() chan *Cell {
+	return self.Without(nil)
+}
+
+func (self *simpleCellList) Without(exclude *Cell) chan *Cell {
 	result := make(chan *Cell, DIM*DIM)
 	if self.cache == nil {
 		self.buildCache()
 	}
 	go func() {
 		for _, cell := range self.cache {
-			result <- cell
-		}
-	}()
-	return result
-}
-
-func (self *simpleCellList) Without(exclude *Cell) chan *Cell {
-	all := self.All()
-	result := make(chan *Cell, DIM*DIM)
-	go func() {
-		for cell := range all {
 			if cell == exclude {
 				continue
 			}
