31014f7cc783167e144c955f409d3d0ae3488446
Alex Komoroske <jkomoros@gmail.com>
2012-12-25 10:35:10 -0500

    TESTS FAIL. Added test for grid creation, which uncovers an error somewhere.

--- dokugen_test.go
+++ dokugen_test.go
@@ -1,6 +1,7 @@
 package dokugen
 
 import (
+	"strings"
 	"testing"
 )
 
@@ -21,6 +22,24 @@ func TestCellCreation(t *testing.T) {
 	//TODO: test failing for values that are too high.
 }
 
-func TestBasic(t *testing.T) {
-	_ = Grid{}
+func TestGridCreation(t *testing.T) {
+	cellData := "1"
+	rowData := strings.Join(nCopies(cellData, DIM), COL_SEP)
+	data := strings.Join(nCopies(rowData, DIM), ROW_SEP)
+	grid := NewGrid(data)
+	if len(grid.cells) != DIM*DIM {
+		t.Log("Didn't generate enough cells")
+		t.Fail()
+	}
+	if grid.cells[10].Number != 1 {
+		t.Log("A random spot check of a cell had the wrong number: %s", grid.cells[10])
+		t.Fail()
+	}
+}
+
+func nCopies(in string, copies int) (result []string) {
+	for i := 0; i < copies; i++ {
+		result = append(result, in)
+	}
+	return
 }
