f2a0d563c388fe744c594ffda207bd8c0010260e
Alex Komoroske <jkomoros@gmail.com>
2012-12-25 10:18:51 -0500

    Added simple tests for Cell.

--- dokugen_test.go
+++ dokugen_test.go
@@ -4,6 +4,23 @@ import (
 	"testing"
 )
 
+func TestCellCreation(t *testing.T) {
+	cell := NewCell(nil, 0, 0, "1")
+	if cell.Number != 1 {
+		t.Log("Number came back wrong")
+		t.Fail()
+	}
+	if cell.Row != 0 {
+		t.Log("Row came back wrong")
+		t.Fail()
+	}
+	if cell.Col != 0 {
+		t.Log("Cell came back wrong")
+		t.Fail()
+	}
+	//TODO: test failing for values that are too high.
+}
+
 func TestBasic(t *testing.T) {
 	_ = Grid{}
 }
