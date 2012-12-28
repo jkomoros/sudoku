3c8fd6ad1efe37a2a28aea116bee2307788451e1
Alex Komoroske <jkomoros@gmail.com>
2012-12-28 11:25:22 -0500

    Added beginning implementation of Neighbors on Cell. However, we need to mark each cell based on which block it's in.

--- cell.go
+++ cell.go
@@ -1,21 +1,45 @@
 package dokugen
 
 import (
+	"log"
 	"strconv"
 )
 
 type Cell struct {
-	grid   *Grid
-	Number int
-	Row    int
-	Col    int
-	cells  []*Cell
+	grid      *Grid
+	Number    int
+	Row       int
+	Col       int
+	neighbors []*Cell
 }
 
 func NewCell(grid *Grid, row int, col int, data string) Cell {
 	//Format, for now, is just the number itself, or 0 if no number.
 	num, _ := strconv.Atoi(data)
 	return Cell{grid, num, row, col, nil}
+}
+
+func (self *Cell) Neighbors() []*Cell {
+	if !self.grid.initalized {
+		log.Println("Neighbors called before the grid had been finalized")
+		return nil
+	}
+	if self.neighbors == nil {
+		//We'll have DIM -1 neighbors for row, col, block
+		self.neighbors = make([]*Cell, (DIM-1)*3)
+		outputIndex := 0
+		for _, cell := range self.grid.Row(self.Row) {
+			self.neighbors[outputIndex] = cell
+			outputIndex++
+		}
+		for _, cell := range self.grid.Col(self.Col) {
+			self.neighbors[outputIndex] = cell
+			outputIndex++
+		}
+		//TODO: also put in each block neighbor.
+	}
+	return self.neighbors
+
 }
 
 func (self *Cell) DataString() string {
