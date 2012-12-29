3cb234e61039ba3b3296bbfa0e2680e638385e86
Alex Komoroske <jkomoros@gmail.com>
2012-12-29 16:43:32 -0500

    Implemented Insert on FiniteList. No new tests yet.

--- finite_queue.go
+++ finite_queue.go
@@ -14,3 +14,39 @@ type FiniteQueue struct {
 func NewFiniteQueue(min int, max int) *FiniteQueue {
 	return &FiniteQueue{min, max, make([][]RankedObject, max-min+1)}
 }
+
+func (self *FiniteQueue) Insert(obj RankedObject) {
+	rank := obj.Rank()
+	list := self.getList(rank)
+	if list == nil {
+		//Apparently rank wasn't legal.
+		return
+	}
+	//Scrub the list for this item.
+	for _, item := range list {
+		if item == obj {
+			//It's already there, just return.
+			return
+		}
+	}
+	//If we get to here we need to add the item.
+	self.setList(rank, append(list, obj))
+}
+
+func (self *FiniteQueue) legalRank(rank int) bool {
+	return rank >= self.min && rank <= self.max
+}
+
+func (self *FiniteQueue) getList(rank int) []RankedObject {
+	if !self.legalRank(rank) {
+		return nil
+	}
+	return self.objects[rank-self.min]
+}
+
+func (self *FiniteQueue) setList(rank int, list []RankedObject) {
+	if !self.legalRank(rank) {
+		return
+	}
+	self.objects[rank-self.min] = list
+}
