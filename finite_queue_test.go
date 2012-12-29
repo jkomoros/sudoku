833477c09dfe3f1877df6208f329149fb03c213c
Alex Komoroske <jkomoros@gmail.com>
2012-12-29 17:51:07 -0500

    TESTS FAIL. Added in another test to change the rank and re-insert. It fails; turns out that we're not putting in pointers.

--- finite_queue_test.go
+++ finite_queue_test.go
@@ -48,7 +48,44 @@ func TestFiniteQueue(t *testing.T) {
 		t.Log("We were able to get back more objects than what we put in.")
 		t.Fail()
 	}
-	//TODO: test changing the rank.
+
+	//Now test changing rank.
+	for _, obj := range objects {
+		queue.Insert(obj)
+	}
+
+	objects[1].rank = 4
+	queue.Insert(objects[1])
+	//Keep track of our golden set, too.
+	temp := objects[1]
+	objects[1] = objects[2]
+	objects[2] = objects[3]
+	objects[3] = temp
+
+	for _, obj := range objects {
+		retrievedObj := queue.Get()
+		if retrievedObj == nil {
+			t.Log("We got back a nil before we were expecting to")
+			t.Fail()
+			continue
+		}
+		if retrievedObj.Rank() != obj.Rank() {
+			t.Log("We got back an object with the wrong rank: ", retrievedObj.Rank(), " is not ", obj.Rank())
+			t.Fail()
+		}
+		convertedObj, _ := retrievedObj.(SimpleRankedObject)
+		//We tried comparing addresses here, but they weren't the same. Why? Are we copying something somewhere?
+		if convertedObj.id != obj.id {
+			//Note that technically the API doesn't require that items with the same rank come back out in the same order.
+			//So this test will fail even in some valid cases.
+			t.Log("We didn't get back the objects we put in.")
+			t.Fail()
+		}
+	}
+	if queue.Get() != nil {
+		t.Log("We were able to get back more objects than what we put in.")
+		t.Fail()
+	}
 	//TODO: test inputting the same object twice.
 	//TODO: test inputting one object, changing its rank, and then getting (to make sure we don't get again).
 }
