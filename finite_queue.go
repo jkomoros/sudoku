04a54566499950390cacda6db681b34cefcd7018
Alex Komoroske <jkomoros@gmail.com>
2012-12-29 16:35:11 -0500

    Made it so the upper bound of FiniteQueue is inclusive.

--- finite_queue.go
+++ finite_queue.go
@@ -6,9 +6,11 @@ type RankedObject interface {
 
 type FiniteQueue struct {
 	min     int
+	max     int
 	objects [][]RankedObject
 }
 
+//Returns a new queue that will work for items with a rank as low as min or as high as max (inclusive)
 func NewFiniteQueue(min int, max int) *FiniteQueue {
-	return &FiniteQueue{min, make([][]RankedObject, max-min)}
+	return &FiniteQueue{min, max, make([][]RankedObject, max-min+1)}
 }
