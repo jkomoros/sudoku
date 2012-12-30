b28989816f61f23ea1c8b1c4da331cb44eb56363
Alex Komoroske <jkomoros@gmail.com>
2012-12-30 12:15:30 -0500

    Add two tests of fillSimpleCells.

--- grid_test.go
+++ grid_test.go
@@ -17,6 +17,16 @@ const TEST_GRID = `6|1|2|.|.|.|4|.|3
 7|2|.|.|8|5|.|3|.
 5|.|1|.|.|.|9|4|7`
 
+const SOLVED_TEST_GRID = `6|1|2|7|5|8|4|9|3
+8|3|5|4|9|6|1|7|2
+9|4|7|2|1|3|8|6|5
+2|5|9|3|6|1|7|8|4
+1|7|3|8|4|9|2|5|6
+4|6|8|5|2|7|3|1|9
+3|9|6|1|7|4|5|2|8
+7|2|4|9|8|5|6|3|1
+5|8|1|6|3|2|9|4|7`
+
 const TEST_GRID_DIAGRAM = `•••|•••|•••||   |   |   ||•••|   |•••
 •6•|•1•|•2•||   | 5 |   ||•4•|   |•3•
 •••|•••|•••||78 |7  |78 ||•••|  9|•••
@@ -204,6 +214,16 @@ func TestGridLoad(t *testing.T) {
 
 	if grid.Diagram() != TEST_GRID_DIAGRAM {
 		t.Log("The grid did not match the expected diagram: \n", grid.Diagram())
+		t.Fail()
+	}
+
+	if num := grid.fillSimpleCells(); num != 45 {
+		t.Log("We filled simple cells on the test grid but didn't get as many as we were expecting: ", num, "/", 45)
+		t.Fail()
+	}
+
+	if grid.DataString() != SOLVED_TEST_GRID {
+		t.Log("After filling simple cells, the grid was not actually solved correctly.")
 		t.Fail()
 	}
 }
