b423af2290fa488281999276d4cded7ee9867fcd
Alex Komoroske <jkomoros@gmail.com>
2013-01-12 14:33:18 -0800

    Finished the test suite for SyncedFiniteQueue.

--- finite_queue_test.go
+++ finite_queue_test.go
@@ -127,6 +127,41 @@ func TestSyncedFiniteQueue(t *testing.T) {
 		t.Fail()
 	}
 
-	//TODO: Test putting in items and getting them out.
+	secondQueue := NewSyncedFiniteQueue(1, DIM)
+	//Note that the first item does not fit in the first bucket on purpose.
+	objects := [...]*SimpleRankedObject{{3, "a"}, {4, "b"}, {4, "c"}, {5, "d"}}
+	for _, object := range objects {
+		secondQueue.In <- object
+	}
+	for _, obj := range objects {
+		retrievedObj := <-secondQueue.Out
+		if retrievedObj == nil {
+			t.Log("We got back a nil before we were expecting to")
+			t.Fail()
+			continue
+		}
+		if retrievedObj.Rank() != obj.Rank() {
+			t.Log("We got back an object with the wrong rank: ", retrievedObj.Rank(), " is not ", obj.Rank())
+			t.Fail()
+		}
+		convertedObj, _ := retrievedObj.(*SimpleRankedObject)
+		//We tried comparing addresses here, but they weren't the same. Why? Are we copying something somewhere?
+		if convertedObj.id != obj.id {
+			//Note that technically the API doesn't require that items with the same rank come back out in the same order.
+			//So this test will fail even in some valid cases.
+			t.Log("We didn't get back the objects we put in.")
+			t.Fail()
+		}
+	}
+
+	select {
+	case <-secondQueue.Out:
+		t.Log("We were able to get something else out of the queue more than we put in.")
+		t.Fail()
+	default:
+		//pass
+	}
+
+	secondQueue.Exit <- true
 
 }
