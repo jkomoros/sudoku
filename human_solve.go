7bdc7001c1d216d7da01da8d2cf5cc1b24191e8f
Alex Komoroske <jkomoros@gmail.com>
2013-01-12 16:44:29 -0800

    A little bit more in Human Solve sketching out the shape of things.

--- human_solve.go
+++ human_solve.go
@@ -2,11 +2,20 @@ package dokugen
 
 type SolveDirections []*SolveStep
 
+type SolveTechnique int
+
+const (
+	ONLY_LEGAL_NUMBER = iota
+	ONLY_LEGAL_POS_ROW
+	ONLY_LEGAL_POS_COL
+	ONLY_LEGAL_POS_BLOCK
+)
+
 type SolveStep struct {
-	Row    int
-	Col    int
-	Num    int
-	Reason string
+	Row       int
+	Col       int
+	Num       int
+	Technique SolveTechnique
 }
 
 func (self *Grid) HumanSolve() *SolveDirections {
