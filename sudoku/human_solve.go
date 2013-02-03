b5275f7e8df81179c2fb68191603bc65be366fe3
Alex Komoroske <jkomoros@gmail.com>
2013-02-03 09:33:55 -0800

    Factored out necessaryInCollection into a function that can be easily changed to be for columns, and blocks as well as rows.

--- sudoku/human_solve.go
+++ sudoku/human_solve.go
@@ -69,27 +69,34 @@ func (self necessaryInRowTechnique) Description(step *SolveStep) string {
 }
 
 func (self necessaryInRowTechnique) Apply(grid *Grid) *SolveStep {
+	getter := func(index int) []*Cell {
+		return grid.Row(index)
+	}
+	return necessaryInCollection(grid, self, getter)
+}
+
+func necessaryInCollection(grid *Grid, technique SolveTechnique, collectionGetter func(index int) []*Cell) *SolveStep {
 	//This will be a random item
 	indexes := rand.Perm(DIM)
 
-	for _, r := range indexes {
-		seenInRow := make([]int, DIM)
-		row := grid.Row(r)
-		for _, cell := range row {
+	for _, i := range indexes {
+		seenInCollection := make([]int, DIM)
+		collection := collectionGetter(i)
+		for _, cell := range collection {
 			for _, possibility := range cell.Possibilities() {
-				seenInRow[possibility-1]++
+				seenInCollection[possibility-1]++
 			}
 		}
 		seenIndexes := rand.Perm(DIM)
 		for _, index := range seenIndexes {
-			seen := seenInRow[index]
+			seen := seenInCollection[index]
 			if seen == 1 {
 				//Okay, we know our target number. Which cell was it?
-				for _, cell := range row {
+				for _, cell := range collection {
 					if cell.Possible(index + 1) {
 						//Found it!
 						cell.SetNumber(index + 1)
-						return &SolveStep{cell.Row, cell.Col, cell.Number(), self}
+						return &SolveStep{cell.Row, cell.Col, cell.Number(), technique}
 					}
 				}
 			}
