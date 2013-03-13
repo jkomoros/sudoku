d34fc905eff3d5b933d0dba79ae07d10bc4e2fed
Alex Komoroske <jkomoros@gmail.com>
2013-03-13 10:52:00 -0500

    Added a more complicated test of TestSubsetIndexes. I'm now confident it works correctly in all reasonable cases.

--- sudoku/human_solve_test.go
+++ sudoku/human_solve_test.go
@@ -429,8 +429,13 @@ func TestSubsetIndexes(t *testing.T) {
 	result := subsetIndexes(3, 1)
 	expectedResult := [][]int{[]int{0}, []int{1}, []int{2}}
 	subsetIndexHelper(t, result, expectedResult)
+
 	result = subsetIndexes(3, 2)
 	expectedResult = [][]int{[]int{0, 1}, []int{0, 2}, []int{1, 2}}
+	subsetIndexHelper(t, result, expectedResult)
+
+	result = subsetIndexes(5, 3)
+	expectedResult = [][]int{[]int{0, 1, 2}, []int{0, 1, 3}, []int{0, 1, 4}, []int{0, 2, 3}, []int{0, 2, 4}, []int{0, 3, 4}, []int{1, 2, 3}, []int{1, 2, 4}, []int{1, 3, 4}, []int{2, 3, 4}}
 	subsetIndexHelper(t, result, expectedResult)
 }
 
