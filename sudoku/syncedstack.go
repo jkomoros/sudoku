629b1135342fe1d1965949b1e3f47d69386f1cfe
Alex Komoroske <jkomoros@gmail.com>
2013-07-04 11:05:04 -0700

    A very basic implementation of Insert and Length for SyncedStack. Tests written and pass.

--- sudoku/syncedstack.go
+++ sudoku/syncedstack.go
@@ -2,6 +2,10 @@ package sudoku
 
 type instructionType int
 
+const (
+	INSERT = iota
+)
+
 type instruction struct {
 	result          chan interface{}
 	instructionType instructionType
@@ -17,4 +21,39 @@ type SyncedStack struct {
 	instructions chan instruction
 	numItems     int
 	firstItem    *stackItem
+}
+
+func NewSyncedStack() *SyncedStack {
+	stack := &SyncedStack{make(chan instruction), 0, nil}
+	go stack.workLoop()
+	return stack
+}
+
+func (self *SyncedStack) Length() int {
+	return self.numItems
+}
+
+func (self *SyncedStack) Dispose() {
+	close(self.instructions)
+}
+
+func (self *SyncedStack) workLoop() {
+	for {
+		instruction := <-self.instructions
+		switch instruction.instructionType {
+		case INSERT:
+			wrappedItem := &stackItem{instruction.item, self.firstItem}
+			self.firstItem = wrappedItem
+			self.numItems++
+			instruction.result <- nil
+		}
+		//Drop other instructions on the floor for now.
+	}
+}
+
+func (self *SyncedStack) Insert(item interface{}) {
+	result := make(chan interface{})
+	self.instructions <- instruction{result, INSERT, item}
+	<-result
+	return
 }
--- sudoku/syncedstack_test.go
+++ sudoku/syncedstack_test.go
@@ -1,1 +1,23 @@
 package sudoku
+
+import (
+	"testing"
+)
+
+func TestBasic(t *testing.T) {
+	stack := NewSyncedStack()
+	if stack == nil {
+		t.Log("Didn't get a stack back.")
+		t.Fail()
+	}
+	if stack.Length() != 0 {
+		t.Log("A new stack did not have a length of one")
+		t.Fail()
+	}
+	item := map[string]int{"a": 1}
+	stack.Insert(item)
+	if stack.Length() != 1 {
+		t.Log("We inserted an item but the length did not go up by one.")
+		t.Fail()
+	}
+}
