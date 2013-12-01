5fb904a461afb0418d6b7b60d6aed66511f4ef63
Alex Komoroske <jkomoros@gmail.com>
2013-11-30 20:45:31 -0500

    Now the main app only generates a puzzle if -g is passed.

--- app/main.go
+++ app/main.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"dokugen/sudoku"
+	"flag"
 )
 
 //TODO: let people pass in puzzles to solve.
 //TODO: let people pass in a filename to export to.
 
+var GENERATE bool
+
 func main() {
-	grid := sudoku.GenerateGrid()
-	print(grid.DataString())
+
+	flag.BoolVar(&GENERATE, "g", false, "if true, will generate a puzzle.")
+
+	flag.Parse()
+
+	if GENERATE {
+		grid := sudoku.GenerateGrid()
+		print(grid.DataString())
+	}
+
 }
