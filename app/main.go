7276f4b03a792a396441b22a0e8ff88922990a9b
Alex Komoroske <jkomoros@gmail.com>
2013-11-30 20:55:50 -0500

    Made it possible to generate multiple puzzles.

--- app/main.go
+++ app/main.go
@@ -10,11 +10,13 @@ import (
 var GENERATE bool
 var HELP bool
 var PUZZLE_TO_SOLVE string
+var NUM int
 
 func main() {
 
 	flag.BoolVar(&GENERATE, "g", false, "if true, will generate a puzzle.")
 	flag.BoolVar(&HELP, "h", false, "If provided, will print help and exit.")
+	flag.IntVar(&NUM, "n", 1, "Number of things to generate")
 	flag.StringVar(&PUZZLE_TO_SOLVE, "s", "", "If provided, will solve the puzzle at the given filename and print solution.")
 
 	flag.Parse()
@@ -25,8 +27,11 @@ func main() {
 	}
 
 	if GENERATE {
-		grid := sudoku.GenerateGrid()
-		print(grid.DataString())
+		for i := 0; i < NUM; i++ {
+			grid := sudoku.GenerateGrid()
+			print(grid.DataString())
+			print("\n\n")
+		}
 		return
 	}
 
