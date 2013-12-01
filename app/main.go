8b4884fa22c733642ddd04de6073c122953a5ddc
Alex Komoroske <jkomoros@gmail.com>
2013-11-30 20:46:29 -0500

    Made it so there's a help mode.

--- app/main.go
+++ app/main.go
@@ -9,12 +9,19 @@ import (
 //TODO: let people pass in a filename to export to.
 
 var GENERATE bool
+var HELP bool
 
 func main() {
 
 	flag.BoolVar(&GENERATE, "g", false, "if true, will generate a puzzle.")
+	flag.BoolVar(&HELP, "h", false, "If provided, will print help and exit.")
 
 	flag.Parse()
+
+	if HELP {
+		flag.PrintDefaults()
+		return
+	}
 
 	if GENERATE {
 		grid := sudoku.GenerateGrid()
