a47075eeef72b678606ec60700b20aeb7df85d0e
Alex Komoroske <jkomoros@gmail.com>
2013-12-24 16:31:42 -0500

    Made it ALL TESTS PASS. HumanSolve will reject techniques that aren't useful (and log a message to stderr, since really that shouldn't happen). Now just need to go through one by one and fix all of the ones that still return non-useful ones--since technically they could be skipping useful ones by bailing early before they really have one.

--- sudoku/human_solve.go
+++ sudoku/human_solve.go
@@ -795,8 +795,13 @@ func (self *Grid) HumanSolve() SolveDirections {
 
 		for i := 0; i < numTechniques; i++ {
 			possibility := <-possibilitiesChan
+
 			if possibility != nil {
-				possibilities = append(possibilities, possibility)
+				if possibility.IsUseful(self) {
+					possibilities = append(possibilities, possibility)
+				} else {
+					log.Println("Rejecting a not useful suggestion: ", possibility)
+				}
 			}
 		}
 
