436a0b2c98c3926b990950f87435f75ee42fec72
Alex Komoroske <jkomoros@gmail.com>
2014-10-11 16:55:37 -0700

    Moved basic hs technique stuff to hs_techniques.go

---
+++ hs_techniques.go
@@ -0,0 +1,194 @@
+package sudoku
+
+type SolveTechnique interface {
+	Name() string
+	Description(*SolveStep) string
+	//IMPORTANT: a step should return a step IFF that step is valid AND the step would cause useful work to be done if applied.
+
+	//Find returns as many steps as it can find in the grid for that technique. This helps ensure that when we pick a step,
+	//it's more likely to be an "easy" step because there will be more of them at any time.
+	Find(*Grid) []*SolveStep
+	IsFill() bool
+	//How difficult a real human would say this technique is. Generally inversely related to how often a real person would pick it. 0.0 to 1.0.
+	Difficulty() float64
+}
+
+type basicSolveTechnique struct {
+	name       string
+	isFill     bool
+	difficulty float64
+}
+
+func init() {
+
+	//TODO: calculate more realistic weights.
+
+	Techniques = []SolveTechnique{
+		hiddenSingleInRow{
+			basicSolveTechnique{
+				"Necessary In Row",
+				true,
+				0.0,
+			},
+		},
+		hiddenSingleInCol{
+			basicSolveTechnique{
+				"Necessary In Col",
+				true,
+				0.0,
+			},
+		},
+		hiddenSingleInBlock{
+			basicSolveTechnique{
+				"Necessary In Block",
+				true,
+				0.0,
+			},
+		},
+		nakedSingleTechnique{
+			basicSolveTechnique{
+				"Only Legal Number",
+				true,
+				5.0,
+			},
+		},
+		pointingPairRow{
+			basicSolveTechnique{
+				"Pointing Pair Row",
+				false,
+				25.0,
+			},
+		},
+		pointingPairCol{
+			basicSolveTechnique{
+				"Pointing Pair Col",
+				false,
+				25.0,
+			},
+		},
+		nakedPairCol{
+			basicSolveTechnique{
+				"Naked Pair Col",
+				false,
+				75.0,
+			},
+		},
+		nakedPairRow{
+			basicSolveTechnique{
+				"Naked Pair Row",
+				false,
+				75.0,
+			},
+		},
+		nakedPairBlock{
+			basicSolveTechnique{
+				"Naked Pair Block",
+				false,
+				85.0,
+			},
+		},
+		nakedTripleCol{
+			basicSolveTechnique{
+				"Naked Triple Col",
+				false,
+				125.0,
+			},
+		},
+		nakedTripleRow{
+			basicSolveTechnique{
+				"Naked Triple Row",
+				false,
+				125.0,
+			},
+		},
+		nakedTripleBlock{
+			basicSolveTechnique{
+				"Naked Triple Block",
+				false,
+				140.0,
+			},
+		},
+		hiddenPairRow{
+			basicSolveTechnique{
+				"Hidden Pair Row",
+				false,
+				300.0,
+			},
+		},
+		hiddenPairCol{
+			basicSolveTechnique{
+				"Hidden Pair Col",
+				false,
+				300.0,
+			},
+		},
+		hiddenPairBlock{
+			basicSolveTechnique{
+				"Hidden Pair Block",
+				false,
+				250.0,
+			},
+		},
+	}
+}
+
+func (self basicSolveTechnique) Name() string {
+	return self.name
+}
+
+func (self basicSolveTechnique) IsFill() bool {
+	return self.isFill
+}
+
+func (self basicSolveTechnique) Difficulty() float64 {
+	return self.difficulty
+}
+
+//This is useful both for hidden and naked subset techniques
+func subsetIndexes(len int, size int) [][]int {
+	//Given size of array to generate subset for, and size of desired subset, returns an array of all subset-indexes to try.
+	//Sanity check
+	if size > len {
+		return nil
+	}
+
+	//returns an array of slices of size size that give you all of the subsets of a list of length len
+	result := make([][]int, 0)
+	counters := make([]int, size)
+	for i, _ := range counters {
+		counters[i] = i
+	}
+	for {
+		innerResult := make([]int, size)
+		for i, counter := range counters {
+			innerResult[i] = counter
+		}
+		result = append(result, innerResult)
+		//Now, increment.
+		//Start at the end and try to increment each counter one.
+		incremented := false
+		for i := size - 1; i >= 0; i-- {
+
+			counter := counters[i]
+			if counter < len-(size-i) {
+				//Found one!
+				counters[i]++
+				incremented = true
+				if i < size-1 {
+					//It was an inner counter; need to set all of the higher counters to one above the one to the left.
+					base := counters[i] + 1
+					for j := i + 1; j < size; j++ {
+						counters[j] = base
+						base++
+					}
+				}
+				break
+			}
+		}
+		//If we couldn't increment any, there's nothing to do.
+		if !incremented {
+			break
+		}
+	}
+	return result
+}
--- human_solve.go
+++ human_solve.go
@@ -7,6 +7,9 @@ import (
 	"strings"
 )
 
+//The actual techniques are intialized in hs_techniques.go, an actually defined in hst_*.go files.
+var Techniques []SolveTechnique
+
 //Worst case scenario, how many times we'd call HumanSolve to get a difficulty.
 const MAX_DIFFICULTY_ITERATIONS = 50
 
@@ -32,152 +35,6 @@ type SolveStep struct {
 	PointerCells CellList
 	Nums         IntSlice
 	Technique    SolveTechnique
-}
-
-type SolveTechnique interface {
-	Name() string
-	Description(*SolveStep) string
-	//IMPORTANT: a step should return a step IFF that step is valid AND the step would cause useful work to be done if applied.
-
-	//Find returns as many steps as it can find in the grid for that technique. This helps ensure that when we pick a step,
-	//it's more likely to be an "easy" step because there will be more of them at any time.
-	Find(*Grid) []*SolveStep
-	IsFill() bool
-	//How difficult a real human would say this technique is. Generally inversely related to how often a real person would pick it. 0.0 to 1.0.
-	Difficulty() float64
-}
-
-type basicSolveTechnique struct {
-	name       string
-	isFill     bool
-	difficulty float64
-}
-
-var Techniques []SolveTechnique
-
-func init() {
-
-	//TODO: calculate more realistic weights.
-
-	Techniques = []SolveTechnique{
-		hiddenSingleInRow{
-			basicSolveTechnique{
-				"Necessary In Row",
-				true,
-				0.0,
-			},
-		},
-		hiddenSingleInCol{
-			basicSolveTechnique{
-				"Necessary In Col",
-				true,
-				0.0,
-			},
-		},
-		hiddenSingleInBlock{
-			basicSolveTechnique{
-				"Necessary In Block",
-				true,
-				0.0,
-			},
-		},
-		nakedSingleTechnique{
-			basicSolveTechnique{
-				"Only Legal Number",
-				true,
-				5.0,
-			},
-		},
-		pointingPairRow{
-			basicSolveTechnique{
-				"Pointing Pair Row",
-				false,
-				25.0,
-			},
-		},
-		pointingPairCol{
-			basicSolveTechnique{
-				"Pointing Pair Col",
-				false,
-				25.0,
-			},
-		},
-		nakedPairCol{
-			basicSolveTechnique{
-				"Naked Pair Col",
-				false,
-				75.0,
-			},
-		},
-		nakedPairRow{
-			basicSolveTechnique{
-				"Naked Pair Row",
-				false,
-				75.0,
-			},
-		},
-		nakedPairBlock{
-			basicSolveTechnique{
-				"Naked Pair Block",
-				false,
-				85.0,
-			},
-		},
-		nakedTripleCol{
-			basicSolveTechnique{
-				"Naked Triple Col",
-				false,
-				125.0,
-			},
-		},
-		nakedTripleRow{
-			basicSolveTechnique{
-				"Naked Triple Row",
-				false,
-				125.0,
-			},
-		},
-		nakedTripleBlock{
-			basicSolveTechnique{
-				"Naked Triple Block",
-				false,
-				140.0,
-			},
-		},
-		hiddenPairRow{
-			basicSolveTechnique{
-				"Hidden Pair Row",
-				false,
-				300.0,
-			},
-		},
-		hiddenPairCol{
-			basicSolveTechnique{
-				"Hidden Pair Col",
-				false,
-				300.0,
-			},
-		},
-		hiddenPairBlock{
-			basicSolveTechnique{
-				"Hidden Pair Block",
-				false,
-				250.0,
-			},
-		},
-	}
-}
-
-func (self basicSolveTechnique) Name() string {
-	return self.name
-}
-
-func (self basicSolveTechnique) IsFill() bool {
-	return self.isFill
-}
-
-func (self basicSolveTechnique) Difficulty() float64 {
-	return self.difficulty
 }
 
 func newFillSolveStep(cell *Cell, num int, technique SolveTechnique) *SolveStep {
@@ -245,53 +102,6 @@ func (self *SolveStep) Description() string {
 		result += fmt.Sprintf("We remove the possibilities %s from cells %s ", self.Nums.Description(), self.TargetCells.Description())
 	}
 	result += "because " + self.Technique.Description(self) + "."
-	return result
-}
-
-func subsetIndexes(len int, size int) [][]int {
-	//Sanity check
-	if size > len {
-		return nil
-	}
-
-	//returns an array of slices of size size that give you all of the subsets of a list of length len
-	result := make([][]int, 0)
-	counters := make([]int, size)
-	for i, _ := range counters {
-		counters[i] = i
-	}
-	for {
-		innerResult := make([]int, size)
-		for i, counter := range counters {
-			innerResult[i] = counter
-		}
-		result = append(result, innerResult)
-		//Now, increment.
-		//Start at the end and try to increment each counter one.
-		incremented := false
-		for i := size - 1; i >= 0; i-- {
-
-			counter := counters[i]
-			if counter < len-(size-i) {
-				//Found one!
-				counters[i]++
-				incremented = true
-				if i < size-1 {
-					//It was an inner counter; need to set all of the higher counters to one above the one to the left.
-					base := counters[i] + 1
-					for j := i + 1; j < size; j++ {
-						counters[j] = base
-						base++
-					}
-				}
-				break
-			}
-		}
-		//If we couldn't increment any, there's nothing to do.
-		if !incremented {
-			break
-		}
-	}
 	return result
 }
 
