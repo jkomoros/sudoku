17b25d1b56b0f2efbc96f415c3329bc00faa160c
Alex Komoroske <jkomoros@gmail.com>
2014-10-11 16:44:42 -0700

    Moved Naked Subset techniques into hst_naked_subset.go

---
+++ hst_naked_subset.go
@@ -0,0 +1,172 @@
+package sudoku
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+type nakedPairCol struct {
+	basicSolveTechnique
+}
+
+type nakedPairRow struct {
+	basicSolveTechnique
+}
+
+type nakedPairBlock struct {
+	basicSolveTechnique
+}
+
+type nakedTripleCol struct {
+	basicSolveTechnique
+}
+
+type nakedTripleRow struct {
+	basicSolveTechnique
+}
+
+type nakedTripleBlock struct {
+	basicSolveTechnique
+}
+
+func (self nakedPairCol) Description(step *SolveStep) string {
+	if len(step.Nums) < 2 || len(step.PointerCells) < 2 {
+		return ""
+	}
+	return fmt.Sprintf("%d and %d are only possible in (%d,%d) and (%d,%d), which means that they can't be in any other cell in column %d", step.Nums[0], step.Nums[1], step.PointerCells[0].Row+1, step.PointerCells[0].Col+1, step.PointerCells[1].Row+1, step.PointerCells[1].Col+1, step.TargetCells.Col())
+}
+
+func (self nakedPairCol) Find(grid *Grid) []*SolveStep {
+	//TODO: test that this will find multiple if they exist.
+	colGetter := func(i int) CellList {
+		return grid.Col(i)
+	}
+	return nakedSubset(grid, self, 2, colGetter)
+}
+
+func (self nakedPairRow) Description(step *SolveStep) string {
+	if len(step.Nums) < 2 || len(step.PointerCells) < 2 {
+		return ""
+	}
+	return fmt.Sprintf("%d and %d are only possible in (%d,%d) and (%d,%d), which means that they can't be in any other cell in row %d", step.Nums[0], step.Nums[1], step.PointerCells[0].Row+1, step.PointerCells[0].Col+1, step.PointerCells[1].Row+1, step.PointerCells[1].Col+1, step.TargetCells.Row())
+}
+
+func (self nakedPairRow) Find(grid *Grid) []*SolveStep {
+	//TODO: test we find multiple if they exist.
+	rowGetter := func(i int) CellList {
+		return grid.Row(i)
+	}
+	return nakedSubset(grid, self, 2, rowGetter)
+}
+
+func (self nakedPairBlock) Description(step *SolveStep) string {
+	if len(step.Nums) < 2 || len(step.PointerCells) < 2 {
+		return ""
+	}
+	return fmt.Sprintf("%d and %d are only possible in (%d,%d) and (%d,%d), which means that they can't be in any other cell in block %d", step.Nums[0], step.Nums[1], step.PointerCells[0].Row+1, step.PointerCells[0].Col+1, step.PointerCells[1].Row+1, step.PointerCells[1].Col+1, step.TargetCells.Block())
+}
+
+func (self nakedPairBlock) Find(grid *Grid) []*SolveStep {
+	//TODO: test that this will return multiple if they exist.
+	blockGetter := func(i int) CellList {
+		return grid.Block(i)
+	}
+	return nakedSubset(grid, self, 2, blockGetter)
+}
+
+func (self nakedTripleCol) Description(step *SolveStep) string {
+	if len(step.Nums) < 3 || len(step.PointerCells) < 3 {
+		return ""
+	}
+	return fmt.Sprintf("%d, %d, and %d are only possible in (%d,%d), (%d,%d) and (%d,%d), which means that they can't be in any other cell in column %d", step.Nums[0], step.Nums[1], step.Nums[2], step.PointerCells[0].Row+1, step.PointerCells[0].Col+1, step.PointerCells[1].Row+1, step.PointerCells[1].Col+1, step.PointerCells[2].Row+1, step.PointerCells[1].Col+1, step.TargetCells.Col())
+}
+
+func (self nakedTripleCol) Find(grid *Grid) []*SolveStep {
+	//TODO: test we find multiple if they exist.
+	colGetter := func(i int) CellList {
+		return grid.Col(i)
+	}
+	return nakedSubset(grid, self, 3, colGetter)
+}
+
+func (self nakedTripleRow) Description(step *SolveStep) string {
+	if len(step.Nums) < 3 || len(step.PointerCells) < 3 {
+		return ""
+	}
+	return fmt.Sprintf("%d, %d, and %d are only possible in (%d,%d), (%d, %d) and (%d,%d), which means that they can't be in any other cell in row %d", step.Nums[0], step.Nums[1], step.Nums[2], step.PointerCells[0].Row+1, step.PointerCells[0].Col+1, step.PointerCells[1].Row+1, step.PointerCells[1].Col+1, step.PointerCells[2].Row+1, step.PointerCells[2].Col+1, step.TargetCells.Row())
+}
+
+func (self nakedTripleRow) Find(grid *Grid) []*SolveStep {
+	//TODO: test that if there are multiple we find them.
+	rowGetter := func(i int) CellList {
+		return grid.Row(i)
+	}
+	return nakedSubset(grid, self, 3, rowGetter)
+}
+
+func (self nakedTripleBlock) Description(step *SolveStep) string {
+	if len(step.Nums) < 3 || len(step.PointerCells) < 3 {
+		return ""
+	}
+	return fmt.Sprintf("%d, %d and %d are only possible in (%d,%d), (%d,%d) and (%d,%d), which means that they can't be in any other cell in block %d", step.Nums[0], step.Nums[1], step.Nums[2], step.PointerCells[0].Row+1, step.PointerCells[0].Col+1, step.PointerCells[1].Row+1, step.PointerCells[1].Col+1, step.PointerCells[2].Row+1, step.PointerCells[2].Col+1, step.TargetCells.Block())
+}
+
+func (self nakedTripleBlock) Find(grid *Grid) []*SolveStep {
+	//TODO: test that this will find multiple ones if they exist.
+	blockGetter := func(i int) CellList {
+		return grid.Block(i)
+	}
+	return nakedSubset(grid, self, 3, blockGetter)
+}
+
+func nakedSubset(grid *Grid, technique SolveTechnique, k int, collectionGetter func(int) CellList) []*SolveStep {
+	//NOTE: very similar implemenation in hiddenSubset.
+	//TODO: randomize order we visit things.
+	var results []*SolveStep
+	for _, i := range rand.Perm(DIM) {
+
+		groups := subsetCellsWithNPossibilities(k, collectionGetter(i))
+
+		if len(groups) > 0 {
+
+			for _, groupIndex := range rand.Perm(len(groups)) {
+
+				group := groups[groupIndex]
+
+				result := &SolveStep{collectionGetter(i).RemoveCells(group), group, group.PossibilitiesUnion(), technique}
+				if result.IsUseful(grid) {
+					results = append(results, result)
+				}
+				//Keep going
+			}
+		}
+
+	}
+	return results
+}
+
+func subsetCellsWithNPossibilities(k int, inputCells CellList) []CellList {
+	//Given a list of cells (often a row, col, or block) and a target group size K,
+	//returns a list of groups of cells of size K where the union of each group's possibility list
+	//is size K.
+
+	//Note: this function has performance O(n!/k!(n - k)!)
+
+	//First, cull any cells with no possibilities to help minimize n
+	cells := inputCells.FilterByHasPossibilities()
+
+	var results []CellList
+
+	for _, indexes := range subsetIndexes(len(cells), k) {
+		//Build up set of all possibilties in this subset.
+		subset := cells.Subset(indexes)
+		union := subset.PossibilitiesUnion()
+		//Okay, we built up the set. Is it the target size?
+		if len(union) == k {
+			results = append(results, subset)
+		}
+	}
+
+	return results
+
+}
---
+++ hst_naked_subset_test.go
@@ -0,0 +1,180 @@
+package sudoku
+
+import (
+	"testing"
+)
+
+func TestNakedPairCol(t *testing.T) {
+	grid := NewGrid()
+	if !grid.LoadFromFile(puzzlePath("nakedpair3.sdk")) {
+		t.Log("Failed to load nakedpair3.sdk")
+		t.Fail()
+	}
+
+	solver := &nakedPairCol{}
+	steps := solver.Find(grid)
+	if len(steps) == 0 {
+		t.Log("The naked pair col didn't find a cell it should have.")
+		t.FailNow()
+	}
+
+	step := steps[0]
+
+	if len(step.TargetCells) != DIM-2 {
+		t.Log("The naked pair col had the wrong number of target cells")
+		t.Fail()
+	}
+	if len(step.PointerCells) != 2 {
+		t.Log("The naked pair col had the wrong number of pointer clles")
+		t.Fail()
+	}
+	if !step.TargetCells.SameCol() || step.TargetCells.Col() != 8 {
+		t.Log("The target cells in the naked pair col were wrong col")
+		t.Fail()
+	}
+	if len(step.Nums) != 2 || !step.Nums.SameContentAs([]int{2, 3}) {
+		t.Log("Naked pair col found the wrong numbers: ", step.Nums)
+		t.Fail()
+	}
+	step.Apply(grid)
+	firstNum := step.Nums[0]
+	secondNum := step.Nums[1]
+	for _, cell := range step.TargetCells {
+		if cell.Possible(firstNum) || cell.Possible(secondNum) {
+			t.Log("Naked Pair col found was not appleid correctly")
+			t.Fail()
+		}
+	}
+
+	grid.Done()
+}
+
+func TestNakedPairRow(t *testing.T) {
+	grid := NewGrid()
+	if !grid.LoadFromFile(puzzlePath("nakedpair3.sdk")) {
+		t.Log("Failed to load nakedpair3.sdk")
+		t.Fail()
+	}
+	grid = grid.transpose()
+	solver := &nakedPairRow{}
+	steps := solver.Find(grid)
+	if len(steps) == 0 {
+		t.Log("The naked pair row didn't find a cell it should have.")
+		t.FailNow()
+	}
+
+	step := steps[0]
+
+	if len(step.TargetCells) != DIM-2 {
+		t.Log("The naked pair row had the wrong number of target cells")
+		t.Fail()
+	}
+	if len(step.PointerCells) != 2 {
+		t.Log("The naked pair row had the wrong number of pointer clles")
+		t.Fail()
+	}
+	if !step.TargetCells.SameRow() || step.TargetCells.Row() != 8 {
+		t.Log("The target cells in the naked pair row were wrong row")
+		t.Fail()
+	}
+	if len(step.Nums) != 2 || !step.Nums.SameContentAs([]int{2, 3}) {
+		t.Log("Naked pair row found the wrong numbers: ", step.Nums)
+		t.Fail()
+	}
+	step.Apply(grid)
+	firstNum := step.Nums[0]
+	secondNum := step.Nums[1]
+	for _, cell := range step.TargetCells {
+		if cell.Possible(firstNum) || cell.Possible(secondNum) {
+			t.Log("Naked Pair row found was not appleid correctly")
+			t.Fail()
+		}
+	}
+
+	grid.Done()
+}
+
+func TestNakedPairBlock(t *testing.T) {
+	grid := NewGrid()
+	grid.Load(NAKED_PAIR_BLOCK_GRID)
+	solver := &nakedPairBlock{}
+	steps := solver.Find(grid)
+	if len(steps) == 0 {
+		t.Log("The naked pair block didn't find a cell it should have.")
+		t.FailNow()
+	}
+
+	step := steps[0]
+
+	if len(step.TargetCells) != DIM-2 {
+		t.Log("The naked pair block had the wrong number of target cells")
+		t.Fail()
+	}
+	if len(step.PointerCells) != 2 {
+		t.Log("The naked pair block had the wrong number of pointer clles")
+		t.Fail()
+	}
+	if !step.TargetCells.SameBlock() || step.TargetCells.Block() != 0 {
+		t.Log("The target cells in the naked pair block were wrong block")
+		t.Fail()
+	}
+	if len(step.Nums) != 2 || !step.Nums.SameContentAs([]int{1, 2}) {
+		t.Log("Naked pair block found the wrong numbers: ", step.Nums)
+		t.Fail()
+	}
+	step.Apply(grid)
+	firstNum := step.Nums[0]
+	secondNum := step.Nums[1]
+	for _, cell := range step.TargetCells {
+		if cell.Possible(firstNum) || cell.Possible(secondNum) {
+			t.Log("Naked Pair block found was not appleid correctly")
+			t.Fail()
+		}
+	}
+
+	grid.Done()
+}
+
+func TestNakedTriple(t *testing.T) {
+	//TODO: test for col and block as well
+	grid := NewGrid()
+	grid.LoadFromFile(puzzlePath("nakedtriplet2.sdk"))
+	solver := &nakedTripleRow{}
+	steps := solver.Find(grid)
+
+	if len(steps) == 0 {
+		t.Log("The naked triple row didn't find a cell it should have.")
+		t.FailNow()
+	}
+
+	step := steps[0]
+
+	if len(step.TargetCells) != DIM-3 {
+		t.Log("The naked triple row had the wrong number of target cells")
+		t.Fail()
+	}
+	if len(step.PointerCells) != 3 {
+		t.Log("The naked triple row had the wrong number of pointer clles")
+		t.Fail()
+	}
+	if !step.TargetCells.SameRow() || step.TargetCells.Row() != 4 {
+		t.Log("The target cells in the naked triple row were wrong row")
+		t.Fail()
+	}
+	if len(step.Nums) != 3 || !step.Nums.SameContentAs([]int{3, 5, 8}) {
+		t.Log("Naked triple row found the wrong numbers: ", step.Nums)
+		t.Fail()
+	}
+	step.Apply(grid)
+	firstNum := step.Nums[0]
+	secondNum := step.Nums[1]
+	thirdNum := step.Nums[2]
+	for _, cell := range step.TargetCells {
+		if cell.Possible(firstNum) || cell.Possible(secondNum) || cell.Possible(thirdNum) {
+			t.Log("Naked triple row found was not appleid correctly")
+			t.Fail()
+		}
+	}
+
+	grid.Done()
+}
