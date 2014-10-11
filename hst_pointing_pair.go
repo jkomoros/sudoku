bebeaff7d0d6def5429a6ad8aa0d4ac3bbc4628e
Alex Komoroske <jkomoros@gmail.com>
2014-10-11 16:47:06 -0700

    Moved poining pair stuff to hst_point_pair.go.

---
+++ hst_pointing_pair.go
@@ -0,0 +1,93 @@
+package sudoku
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+type pointingPairRow struct {
+	basicSolveTechnique
+}
+
+type pointingPairCol struct {
+	basicSolveTechnique
+}
+
+func (self pointingPairRow) Description(step *SolveStep) string {
+	if len(step.Nums) == 0 {
+		return ""
+	}
+	return fmt.Sprintf("%d is only possible in row %d of block %d, which means it can't be in any other cell in that row not in that block", step.Nums[0], step.TargetCells.Row(), step.PointerCells.Block())
+}
+
+func (self pointingPairRow) Find(grid *Grid) []*SolveStep {
+	//Within each block, for each number, see if all items that allow it are aligned in a row or column.
+	//TODO: randomize order of blocks.
+	//TODO: this is substantially duplicated in pointingPaircol
+	//TODO: test this returns multiple if they exist.
+
+	var results []*SolveStep
+
+	for _, i := range rand.Perm(DIM) {
+		block := grid.Block(i)
+
+		for _, num := range rand.Perm(DIM) {
+			cells := block.FilterByPossible(num + 1)
+			//cellList is now a list of all cells that have that number.
+			if len(cells) <= 1 || len(cells) > BLOCK_DIM {
+				//Meh, not a match.
+				continue
+			}
+			//Okay, it's possible it's a match. Are all rows the same?
+			if cells.SameRow() {
+				//Yup!
+				result := &SolveStep{grid.Row(cells.Row()).RemoveCells(block), cells, []int{num + 1}, self}
+				if result.IsUseful(grid) {
+					results = append(results, result)
+				}
+				//Keep looking for more!
+			}
+		}
+	}
+	return results
+}
+
+func (self pointingPairCol) Description(step *SolveStep) string {
+	if len(step.Nums) == 0 {
+		return ""
+	}
+	return fmt.Sprintf("%d is only possible in column %d of block %d, which means it can't be in any other cell in that column not in that block", step.Nums[0], step.TargetCells.Col(), step.PointerCells.Block())
+}
+
+func (self pointingPairCol) Find(grid *Grid) []*SolveStep {
+	//Within each block, for each number, see if all items that allow it are aligned in a row or column.
+	//TODO: randomize order of blocks.
+	//TODO: this is substantially duplicated in pointingPairRow
+	//TODO: test this will find multiple if they exist.
+
+	var results []*SolveStep
+
+	for _, i := range rand.Perm(DIM) {
+		block := grid.Block(i)
+		//TODO: randomize order of numbers to test for.
+
+		for _, num := range rand.Perm(DIM) {
+			cells := block.FilterByPossible(num + 1)
+			//cellList is now a list of all cells that have that number.
+			if len(cells) <= 1 || len(cells) > BLOCK_DIM {
+				//Meh, not a match.
+				continue
+			}
+			//Okay, are all cols?
+			if cells.SameCol() {
+				//Yup!
+				result := &SolveStep{grid.Col(cells.Col()).RemoveCells(block), cells, []int{num + 1}, self}
+				if result.IsUseful(grid) {
+					results = append(results, result)
+				}
+				//Keep looking!
+			}
+		}
+	}
+	return results
+}
---
+++ hst_pointing_pair_test.go
@@ -0,0 +1,85 @@
+package sudoku
+
+import (
+	"testing"
+)
+
+func TestPointingPairCol(t *testing.T) {
+	grid := NewGrid()
+	grid.Load(POINTING_PAIR_COL_GRID)
+	solver := &pointingPairCol{}
+	steps := solver.Find(grid)
+	if len(steps) == 0 {
+		t.Log("The pointing pair col didn't find a cell it should have")
+		t.FailNow()
+	}
+
+	step := steps[0]
+
+	if len(step.TargetCells) != BLOCK_DIM*2 {
+		t.Log("The pointing pair col gave back the wrong number of target cells")
+		t.Fail()
+	}
+	if len(step.PointerCells) != BLOCK_DIM-1 {
+		t.Log("The pointing pair col gave back the wrong number of pointer cells")
+		t.Fail()
+	}
+	if !step.TargetCells.SameCol() || step.TargetCells.Col() != 1 {
+		t.Log("The target cells in the pointing pair col technique were wrong col")
+		t.Fail()
+	}
+	if len(step.Nums) != 1 || step.Nums[0] != 7 {
+		t.Log("Pointing pair col technique gave the wrong number")
+		t.Fail()
+	}
+	step.Apply(grid)
+	num := step.Nums[0]
+	for _, cell := range step.TargetCells {
+		if cell.Possible(num) {
+			t.Log("The pointing pairs col technique was not applied correclty")
+			t.Fail()
+		}
+	}
+
+	grid.Done()
+}
+
+func TestPointingPairRow(t *testing.T) {
+	grid := NewGrid()
+	grid.Load(POINTING_PAIR_ROW_GRID)
+	solver := &pointingPairRow{}
+	steps := solver.Find(grid)
+	if len(steps) == 0 {
+		t.Log("The pointing pair row didn't find a cell it should have")
+		t.FailNow()
+	}
+
+	step := steps[0]
+
+	if len(step.TargetCells) != BLOCK_DIM*2 {
+		t.Log("The pointing pair row gave back the wrong number of target cells")
+		t.Fail()
+	}
+	if len(step.PointerCells) != BLOCK_DIM-1 {
+		t.Log("The pointing pair row gave back the wrong number of pointer cells")
+		t.Fail()
+	}
+	if !step.TargetCells.SameRow() || step.TargetCells.Row() != 1 {
+		t.Log("The target cells in the pointing pair row technique were wrong row")
+		t.Fail()
+	}
+	if len(step.Nums) != 1 || step.Nums[0] != 7 {
+		t.Log("Pointing pair row technique gave the wrong number")
+		t.Fail()
+	}
+	step.Apply(grid)
+	num := step.Nums[0]
+	for _, cell := range step.TargetCells {
+		if cell.Possible(num) {
+			t.Log("The pointing pairs row technique was not applied correclty")
+			t.Fail()
+		}
+	}
+
+	grid.Done()
+}
