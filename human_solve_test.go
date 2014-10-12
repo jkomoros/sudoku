542036bfa03c3f9b0e53d6471a49b75b5e0aea13
Alex Komoroske <jkomoros@gmail.com>
2014-10-12 11:03:49 -0700

    Moved TestSubsetIndexes to hs_techniques_test.go

--- human_solve_test.go
+++ human_solve_test.go
@@ -5,45 +5,6 @@ import (
 	"time"
 )
 
-func TestSubsetIndexes(t *testing.T) {
-	result := subsetIndexes(3, 1)
-	expectedResult := [][]int{[]int{0}, []int{1}, []int{2}}
-	subsetIndexHelper(t, result, expectedResult)
-
-	result = subsetIndexes(3, 2)
-	expectedResult = [][]int{[]int{0, 1}, []int{0, 2}, []int{1, 2}}
-	subsetIndexHelper(t, result, expectedResult)
-
-	result = subsetIndexes(5, 3)
-	expectedResult = [][]int{[]int{0, 1, 2}, []int{0, 1, 3}, []int{0, 1, 4}, []int{0, 2, 3}, []int{0, 2, 4}, []int{0, 3, 4}, []int{1, 2, 3}, []int{1, 2, 4}, []int{1, 3, 4}, []int{2, 3, 4}}
-	subsetIndexHelper(t, result, expectedResult)
-
-	if subsetIndexes(1, 2) != nil {
-		t.Log("Subset indexes returned a subset where the length is greater than the len")
-		t.Fail()
-	}
-
-}
-
-func subsetIndexHelper(t *testing.T, result [][]int, expectedResult [][]int) {
-	if len(result) != len(expectedResult) {
-		t.Log("subset indexes returned wrong number of results for: ", result, " :", expectedResult)
-		t.FailNow()
-	}
-	for i, item := range result {
-		if len(item) != len(expectedResult[0]) {
-			t.Log("subset indexes returned a result with wrong numbrer of items ", i, " : ", result, " : ", expectedResult)
-			t.FailNow()
-		}
-		for j, value := range item {
-			if value != expectedResult[i][j] {
-				t.Log("Subset indexes had wrong number at ", i, ",", j, " : ", result, " : ", expectedResult)
-				t.Fail()
-			}
-		}
-	}
-}
-
 func BenchmarkHumanSolve(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		grid := NewGrid()
