a9d5ef0fd7f5e96fbdeddfdbe77296121fb4f905
Alex Komoroske <jkomoros@gmail.com>
2014-11-06 16:18:18 -0800

    Fixed a bug in the test for guess where we were looking for one of the wrong descriptions.

--- hst_guess_test.go
+++ hst_guess_test.go
@@ -12,7 +12,7 @@ func TestGuessTechnique(t *testing.T) {
 		targetNums:  IntSlice([]int{3, 7}),
 		descriptions: []string{
 			"we have no other moves to make, so we randomly pick a cell with the smallest number of possibilities, (0,0), and pick one of its possibilities",
-			"we have no other moves to make, so we randomly pick a cell with the smallest number of possibilities, (0,1), and pick one of its possibilities"},
+			"we have no other moves to make, so we randomly pick a cell with the smallest number of possibilities, (1,0), and pick one of its possibilities"},
 	}
 	humanSolveTechniqueTestHelper(t, "guesstestgrid.sdk", "Guess", options)
 }
