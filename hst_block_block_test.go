a3161f51c40115bbba4052bfb32b16eada3c8a71
Alex Komoroske <jkomoros@gmail.com>
2014-11-16 23:28:04 -0500

    TESTS FAIL. Broke them into two methods so they will both fail if they should.

--- hst_block_block_test.go
+++ hst_block_block_test.go
@@ -25,6 +25,7 @@ func TestPairwiseBlocks(t *testing.T) {
 
 func TestBlockBlockInteraction(t *testing.T) {
 	//TODO: test description
+
 	options := solveTechniqueTestHelperOptions{
 		targetCells:  []cellRef{{6, 3}, {6, 4}, {7, 3}, {7, 4}, {8, 3}, {8, 4}},
 		pointerCells: []cellRef{{2, 3}, {2, 4}, {3, 4}, {5, 3}},
@@ -34,7 +35,10 @@ func TestBlockBlockInteraction(t *testing.T) {
 	}
 	humanSolveTechniqueTestHelper(t, "blockblocktest.sdk", "Block Block Interactions", options)
 
-	options = solveTechniqueTestHelperOptions{
+}
+
+func TestBlockBlockInteractionAgain(t *testing.T) {
+	options := solveTechniqueTestHelperOptions{
 		targetCells:  []cellRef{{3, 3}, {3, 4}, {3, 5}, {5, 3}, {5, 4}, {5, 5}},
 		pointerCells: []cellRef{{3, 0}, {3, 1}, {3, 6}, {3, 7}, {5, 0}, {5, 1}, {5, 6}, {5, 7}},
 		targetSame:   GROUP_BLOCK,
@@ -42,5 +46,4 @@ func TestBlockBlockInteraction(t *testing.T) {
 		targetNums:   IntSlice([]int{2}),
 	}
 	humanSolveTechniqueTestHelper(t, "blockblocktest1.sdk", "Block Block Interactions", options)
-
 }
