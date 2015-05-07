7c7c731834ef3bfc7d3ad8d0503f1f0d79cb50ff
Alex Komoroske <jkomoros@gmail.com>
2015-05-06 20:53:40 -0700

    Pointing Pair now implements new Find signature

--- hst_pointing_pair.go
+++ hst_pointing_pair.go
@@ -31,16 +31,21 @@ func (self *pointingPairTechnique) Description(step *SolveStep) string {
 	return fmt.Sprintf("%d is only possible in %s %d of block %d, which means it can't be in any other cell in that %s not in that block", step.TargetNums[0], groupName, groupNum, step.PointerCells.Block(), groupName)
 }
 
-func (self *pointingPairTechnique) Find(grid *Grid) []*SolveStep {
+func (self *pointingPairTechnique) Find(grid *Grid, results chan *SolveStep, done chan bool) {
 	//Within each block, for each number, see if all items that allow it are aligned in a row or column.
 	//TODO: test this returns multiple if they exist.
-
-	var results []*SolveStep
 
 	for _, i := range rand.Perm(DIM) {
 		block := grid.Block(i)
 
 		for _, num := range rand.Perm(DIM) {
+
+			select {
+			case <-done:
+				return
+			default:
+			}
+
 			cells := block.FilterByPossible(num + 1)
 			//cellList is now a list of all cells that have that number.
 			if len(cells) <= 1 || len(cells) > BLOCK_DIM {
@@ -50,18 +55,21 @@ func (self *pointingPairTechnique) Find(grid *Grid) []*SolveStep {
 			//Okay, it's possible it's a match. Are all relevant groups (rows or cols, depending on groupType) the same?
 			if (self.groupType == _GROUP_ROW && cells.SameRow()) || (self.groupType == _GROUP_COL && cells.SameCol()) {
 				//Yup!
-				var result *SolveStep
+				var step *SolveStep
 				if self.groupType == _GROUP_ROW {
-					result = &SolveStep{self, grid.Row(cells.Row()).RemoveCells(block), []int{num + 1}, cells, nil}
+					step = &SolveStep{self, grid.Row(cells.Row()).RemoveCells(block), []int{num + 1}, cells, nil}
 				} else {
-					result = &SolveStep{self, grid.Col(cells.Col()).RemoveCells(block), []int{num + 1}, cells, nil}
+					step = &SolveStep{self, grid.Col(cells.Col()).RemoveCells(block), []int{num + 1}, cells, nil}
 				}
-				if result.IsUseful(grid) {
-					results = append(results, result)
+				if step.IsUseful(grid) {
+					select {
+					case results <- step:
+					case <-done:
+						return
+					}
 				}
 				//Keep looking for more!
 			}
 		}
 	}
-	return results
 }
