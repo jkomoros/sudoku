39af5db04a271c0a7e3c15d281e73652760db1cd
Alex Komoroske <jkomoros@gmail.com>
2015-05-10 21:05:26 -0700

    TESTS FAIL. Added the forcing chain test, but it doesn't yet pass.

--- hst_forcing_chains_test.go
+++ hst_forcing_chains_test.go
@@ -5,5 +5,11 @@ import (
 )
 
 func TestForcingChains(t *testing.T) {
-	//TODO: implement testing here.
+	//TODO: test description
+	options := solveTechniqueTestHelperOptions{
+		targetCells: []cellRef{{0, 1}},
+		targetNums:  IntSlice([]int{7}),
+		debugPrint:  true,
+	}
+	humanSolveTechniqueTestHelper(t, "forcingchain_test1.sdk", "Forcing Chain", options)
 }
