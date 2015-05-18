39a4fa3e34bebaa54e165114c321a7c766e9fac6
Alex Komoroske <jkomoros@gmail.com>
2015-05-17 17:57:11 -0700

    Added more notes about how to fix the tests

--- hst_forcing_chains_test.go
+++ hst_forcing_chains_test.go
@@ -9,6 +9,8 @@ func TestForcingChains(t *testing.T) {
 	//Steps to test this:
 	//* create a stepsToCheck list of steps we can pass in to humanSolveTEchniqueHelper,
 	//so we can calculate it once and pass that in to the helper.
+	//    * actually, we should split out the helperPrep and helperMeat so that you can call either bit
+	//    but still get the benefit of caching techinques.
 	//* In the forcing chain helper, calculate the steps once, then
 	//pass them in each time in a list of ~10 calls to solveTechniqueTEstHelper that we know are valid here.
 	//* VERIFY MANUALLY that each step that is returned is actually a valid application of forcingchains.
