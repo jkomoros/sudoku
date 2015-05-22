f799cc5083f4eabf9b2e5dbb409b980f52102438
Alex Komoroske <jkomoros@gmail.com>
2015-05-21 19:49:51 -0700

    TESTS FAIL because we added back in another technique that does check out.

--- hst_forcing_chains_test.go
+++ hst_forcing_chains_test.go
@@ -114,7 +114,7 @@ func TestForcingChains(t *testing.T) {
 		//Oh, we fail as soon as we notice they don't all match.
 		//We haven't seen this set again... flakey?
 		//Next step: do the manual check for a 'normal' run to see which is missing
-		// 0,1 / 7 / 5,1 / 1,2 *
+
 		// 0,6 / 3 / 5,1 / 1,2 *
 		//8,3 / 7 / 8,7 / 1,2 *
 		// 0,1 /7 / 5,4 / 2,3 *
@@ -170,6 +170,12 @@ func TestForcingChains(t *testing.T) {
 			targetCells:  []cellRef{{0, 6}},
 			targetNums:   IntSlice([]int{3}),
 			pointerCells: []cellRef{{1, 0}},
+			pointerNums:  IntSlice([]int{1, 2}),
+		},
+		{
+			targetCells:  []cellRef{{0, 1}},
+			targetNums:   IntSlice([]int{7}),
+			pointerCells: []cellRef{{5, 1}},
 			pointerNums:  IntSlice([]int{1, 2}),
 		},
 	}
