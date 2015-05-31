c50c10eceec1d9351b47e4fb1e47572dee3b3e41
Alex Komoroske <jkomoros@gmail.com>
2015-05-31 08:18:22 -0700

    Switched to using multiple folds to validate. But... it returns really fast, with really weird r2 numbers...

--- cmd/dokugen-analysis/util/weights.py
+++ cmd/dokugen-analysis/util/weights.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 from sklearn import datasets, linear_model
+from sklearn.cross_validation import cross_val_score, KFold
 
 import csv
 
@@ -28,29 +29,23 @@ for row in reader:
 targets = np.array(targets_basic)
 data = np.array(data_basic)
 
-#TODO: do folds of test/training sets.
-
-# Split the data into training/testing sets
-data_train = data[:-20]
-data_test = data[-20:]
-
-#Split the targets into training/testing sets
-targets_train = targets[:-20]
-targets_test = targets[-20:]
-
-
 # Create linear regression object
 regr = linear_model.Ridge(alpha=1.0)
 
 # Train the model using the training sets
-regr.fit(data_train, targets_train)
+regr.fit(data, targets)
 
 # The coefficients
 print('Coefficients: \n', regr.coef_)
-# The mean square error
-print("Residual sum of squares: %.2f"
-      % np.mean((regr.predict(data_test) - targets_test) ** 2))
-# Explained variance score: 1 is perfect prediction
-print('Variance score: %.2f' % regr.score(data_test, targets_test))
+# # The mean square error
+# print("Residual sum of squares: %.2f"
+#       % np.mean((regr.predict(data_test) - targets_test) ** 2))
+# # Explained variance score: 1 is perfect prediction
+# print('Variance score: %.2f' % regr.score(data_test, targets_test))
+
+K = 20 #folds
+R2 = cross_val_score(regr, data, y=targets, cv=KFold(targets.size, K)).mean()
+print("The %d-Folds estimate of the coefficient of determination is R2 = %s"
+      % (K, R2))
 
 #TODO: export the model in a way that Go can run it
