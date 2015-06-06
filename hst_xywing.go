e488485d8d90e77d924e4f16ddc7c4f7f8fd4be2
Alex Komoroske <jkomoros@gmail.com>
2015-06-06 16:09:56 -0700

    Initial untested (but building) implementation of xywing technique. Nothing obvious about it that I expect not to work when tested.

--- hst_xywing.go
+++ hst_xywing.go
@@ -16,6 +16,97 @@ func (self *xywingTechnique) Description(step *SolveStep) string {
 
 func (self *xywingTechnique) Find(grid *Grid, results chan *SolveStep, done chan bool) {
 
-	//TODO: implement this.
-	return
+	getter := grid.queue().DefaultGetter()
+
+	for {
+
+		//Check if it's time to stop.
+		select {
+		case <-done:
+			return
+		default:
+		}
+
+		pivot := getter.GetSmallerThan(3)
+
+		if pivot == nil {
+			break
+		}
+
+		pivotCell := pivot.(*Cell)
+
+		if len(pivotCell.Possibilities()) != 2 {
+			//We found one with 1 possibility, which isn't interesting for us--nakedSingle should do that one.
+			continue
+		}
+
+		//OK, this is our pivot cell. Let's consider its two possibilities to
+		//be X and Y.
+
+		x := pivotCell.Possibilities()[0]
+		y := pivotCell.Possibilities()[1]
+
+		//We want to find two neighbors of the pivot cell that have two
+		//possibilities that are XZ and YZ. So we're looking for neighbors
+		//that have two possibilities, one of which is either x or y.
+
+		xList := pivotCell.Neighbors().FilterByPossible(x).FilterByNumPossibilities(2)
+		yList := pivotCell.Neighbors().FilterByPossible(y).FilterByNumPossibilities(2)
+
+		//Now, we'll check for each possible value of Z
+		for z := 0; z < DIM; z++ {
+			//z can't be either x or y, so don't do that work
+			if z == x || z == y {
+				continue
+			}
+
+			zFilteredXList := xList.FilterByPossible(z)
+			zFilteredYList := yList.FilterByPossible(z)
+
+			//If there area any items left, we've found candidates.
+			if len(zFilteredXList) == 0 || len(zFilteredYList) == 0 {
+				//TOOD: we can save a tiny bit of work by checking if
+				//zFilteredXList is len() 0 before calculating zFilteredYList
+				continue
+			}
+
+			//Okay, now the two-up combinations of all cells in the
+			//two lists are all candidates. Often it will only be 1
+			//cell in each list, but we'll general case this.
+			for _, xCell := range zFilteredXList {
+				for _, yCell := range zFilteredYList {
+					//xCell, yCell is a possible pointing pair group.
+
+					//find cells that are in both neighbor lists
+					intersection := xCell.Neighbors().toCellSet().intersection(yCell.Neighbors().toCellSet()).toSlice(grid)
+
+					//TODO: consider if we actually need to remove pivotCell;
+					//it might never be able to be in the list anyway.
+					intersection = intersection.RemoveCells(CellSlice{pivotCell})
+
+					affectedCells := intersection.FilterByPossible(z)
+
+					//Okay, we have a candidate step. Is it useful?
+					step := &SolveStep{self,
+						affectedCells,
+						IntSlice{z},
+						CellSlice{pivotCell, xCell, yCell},
+						IntSlice{x, y, z},
+						nil,
+					}
+
+					if step.IsUseful(grid) {
+						select {
+						case results <- step:
+						case <-done:
+							return
+						}
+					}
+
+				}
+			}
+
+		}
+
+	}
 }
