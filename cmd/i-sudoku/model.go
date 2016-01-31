0cb427235abfe22a30f018575935d20a4f5a8480
Alex Komoroske <jkomoros@gmail.com>
2016-01-31 13:13:08 -0500

    Define markMutator and modelMutator, and use that to power SetMarks behind the scenes.

--- cmd/i-sudoku/model.go
+++ cmd/i-sudoku/model.go
@@ -8,18 +8,23 @@ type model struct {
 	grid *sudoku.Grid
 }
 
+type modelMutator interface {
+	Apply(m *model)
+	//TODO: add an Undo method
+}
+
+type markMutator struct {
+	row, col    int
+	marksToggle map[int]bool
+}
+
 func (m *model) SetGrid(grid *sudoku.Grid) {
 	m.grid = grid
 }
 
 func (m *model) SetMarks(row, col int, marksToggle map[int]bool) {
-	cell := m.grid.Cell(row, col)
-	if cell == nil {
-		return
-	}
-	for key, value := range marksToggle {
-		cell.SetMark(key, value)
-	}
+	mutator := &markMutator{row, col, marksToggle}
+	mutator.Apply(m)
 }
 
 func (m *model) SetNumber(row, col int, num int) {
@@ -28,4 +33,14 @@ func (m *model) SetNumber(row, col int, num int) {
 		return
 	}
 	cell.SetNumber(num)
+}
+
+func (m *markMutator) Apply(model *model) {
+	cell := model.grid.Cell(m.row, m.col)
+	if cell == nil {
+		return
+	}
+	for key, value := range m.marksToggle {
+		cell.SetMark(key, value)
+	}
 }
