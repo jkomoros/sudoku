696d9215fce5278f52b409f4e452ecd40bebcd2c
Alex Komoroske <jkomoros@gmail.com>
2016-02-28 23:40:30 -0600

    A working CLI invocation captured. A step towards #150

--- cmd/dokugen-analysis/util/weka.py
+++ cmd/dokugen-analysis/util/weka.py
@@ -9,3 +9,5 @@
 
 # java -cp "/Applications/weka-3-6-11-oracle-jvm.app/Contents/Java/weka.jar" weka.core.converters.CSVLoader solves.csv > solves.arff
 # java <CLASSPATH> weka.classifiers.functions.SMOreg -C 1.0 -N 2 -I "weka.classifiers.functions.supportVector.RegSMOImproved -L 0.001 -W 1 -P 1.0E-12 -T 0.001 -V" -K "weka.classifiers.functions.supportVector.PolyKernel -C 250007 -E 1.0" -c first -i <ARFF FILE>
+
+#java -cp "/Applications/weka-3-6-11-oracle-jvm.app/Contents/Java/weka.jar"
