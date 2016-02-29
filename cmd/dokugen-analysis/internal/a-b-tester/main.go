cacf89880564f06cd2b2f30086699a72370decfd
Alex Komoroske <jkomoros@gmail.com>
2016-02-29 12:18:14 -0800

    Added stub of flags

--- cmd/dokugen-analysis/internal/a-b-tester/main.go
+++ cmd/dokugen-analysis/internal/a-b-tester/main.go
@@ -1,9 +1,40 @@
 package main
 
 import (
+	"flag"
 	"log"
+	"os"
 )
 
+type appOptions struct {
+	message string
+	help    bool
+	flagSet *flag.FlagSet
+}
+
+func (a *appOptions) defineFlags() {
+	if a.flagSet == nil {
+		return
+	}
+	a.flagSet.StringVar(&a.message, "m", "Hello, world!", "The message to print to the screen")
+	a.flagSet.BoolVar(&a.help, "h", false, "If provided, will print help and exit.")
+}
+
+func (a *appOptions) parse(args []string) {
+	a.flagSet.Parse(args)
+}
+
+func newAppOptions(flagSet *flag.FlagSet) *appOptions {
+	a := &appOptions{
+		flagSet: flagSet,
+	}
+	a.defineFlags()
+	return a
+}
+
 func main() {
-	log.Println("hello world")
+	a := newAppOptions(flag.CommandLine)
+	a.parse(os.Args[1:])
+
+	log.Println(a.message)
 }
