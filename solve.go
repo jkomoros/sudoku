a86af0d44d6456c70afa557c0ae2c014bab38281
Alex Komoroske <jkomoros@gmail.com>
2016-08-26 17:15:14 -0700

    Factred it out so nOrFewerSolutions takes a Grid. Also created cachedNOrFewerSolutions, which takes a mutableGridImpl.

--- solve.go
+++ solve.go
@@ -38,7 +38,7 @@ func (self *gridImpl) HasSolution() bool {
 
 func (self *mutableGridImpl) HasSolution() bool {
 	//TODO: optimize this to bail as soon as we find a single solution.
-	return len(self.nOrFewerSolutions(1)) > 0
+	return len(cachedNOrFewerSolutions(self, 1)) > 0
 }
 
 func (self *gridImpl) HasMultipleSolutions() bool {
@@ -47,7 +47,7 @@ func (self *gridImpl) HasMultipleSolutions() bool {
 }
 
 func (self *mutableGridImpl) HasMultipleSolutions() bool {
-	return len(self.nOrFewerSolutions(2)) >= 2
+	return len(cachedNOrFewerSolutions(self, 2)) >= 2
 }
 
 func (self *gridImpl) Solutions() []Grid {
@@ -56,122 +56,21 @@ func (self *gridImpl) Solutions() []Grid {
 }
 
 func (self *mutableGridImpl) Solutions() (solutions []Grid) {
-	return self.nOrFewerSolutions(0)
+	return cachedNOrFewerSolutions(self, 0)
 }
 
-//The actual workhorse of solutions generating. 0 means "as many as you can
-//find". It might return more than you asked for, if it already had more
-//results than requested sitting around.
-func (self *mutableGridImpl) nOrFewerSolutions(max int) []Grid {
-
+func cachedNOrFewerSolutions(self *mutableGridImpl, max int) []Grid {
 	self.cachedSolutionsLockRef.RLock()
 	hasNoCachedSolutions := self.cachedSolutionsRef == nil
 	cachedSolutionsLen := self.cachedSolutionsRequestedLengthRef
 	self.cachedSolutionsLockRef.RUnlock()
 
 	if hasNoCachedSolutions || (max == 0 && cachedSolutionsLen != 0) || (max > 0 && cachedSolutionsLen < max && cachedSolutionsLen > 0) {
-
-		queueDone := make(chan bool, 1)
-
-		queue := newSyncedFiniteQueue(0, DIM*DIM, queueDone)
-
-		queue.In <- self.Copy()
-
-		//In the past this wasn't buffered, but then when we finished early other items would try to go into it
-		//and block, which prevented them from looping back up and getting the signal to shut down.
-		//Since there's only a known number of threads, we'll make sure they all ahve a place to leave their work
-		//without blocking so they can get the signal to shut down.
-		incomingSolutions := make(chan Grid, _NUM_SOLVER_THREADS)
-
-		//Using a pattern for closing fan in style receivers from http://blog.golang.org/pipelines
-		var wg sync.WaitGroup
-
-		var solutions []Grid
-
-		//TODO: figure out a way to kill all of these threads when necessary.
-
-		//TODO: don't use a constant here, use someting around the lines of
-		//numCPU
-
-		for i := 0; i < _NUM_SOLVER_THREADS; i++ {
-			go func() {
-				//Sovler thread.
-				firstRun := true
-				defer wg.Done()
-				for {
-					grid, ok := <-queue.Out
-					if !ok {
-						return
-					}
-					result := searchGridSolutions(grid.(Grid), queue, firstRun, max)
-					if result != nil {
-						incomingSolutions <- result
-					}
-					queue.ItemDone <- true
-					firstRun = false
-				}
-			}()
-		}
-
-		wg.Add(_NUM_SOLVER_THREADS)
-
-		go func() {
-			wg.Wait()
-			close(incomingSolutions)
-		}()
-
-	OuterLoop:
-		for {
-			select {
-			case solution := <-incomingSolutions:
-				//incomingSolutions must have been closed because no more work to do.
-				if solution == nil {
-					break OuterLoop
-				}
-				//Add it to results
-				solutions = append(solutions, solution)
-				if max > 0 && len(solutions) >= max {
-					break OuterLoop
-				}
-			case <-queueDone:
-				//Well, that's as good as it's going to get.
-				break OuterLoop
-			}
-		}
-
-		//In some cases that previous select would have something incoming on
-		//incomingsolutions, as well as on queueDone, and queueDone would have
-		//just so happened to have won. Check for one last remaining item
-		//coming in from incomingSolutions. Technically it's possible (how?)
-		//to have multiple items waiting on incomingSolutions, so read as many
-		//as we can get without blocking.(Not checking for this was the reason
-		//for bug #134.)
-
-	DoneReading:
-		for {
-			select {
-			case solution := <-incomingSolutions:
-				if solution != nil {
-					solutions = append(solutions, solution)
-				}
-			default:
-				//Nope, guess there wasn't one left.
-				break DoneReading
-			}
-		}
-
-		//There might be some things waiting to go into incomingSolutions here, but because it has a slot
-		//for every thread to be buffered, it's OK, we can just stop now.
-
-		//TODO: the grids waiting in the queue will never have their .Done called. This isn't a big deal--GC should reclaim them--
-		//but we won't have as many that we could reuse.
-		queue.Exit <- true
-
+		solutions := nOrFewerSolutions(self, max)
 		self.cachedSolutionsLockRef.Lock()
 		self.cachedSolutionsRef = solutions
 		self.cachedSolutionsRequestedLengthRef = max
 		self.cachedSolutionsLockRef.Unlock()
-
 	}
 
 	//TODO: rejigger this to not need a write lock then a read lock when setting.
@@ -179,6 +78,110 @@ func (self *mutableGridImpl) nOrFewerSolutions(max int) []Grid {
 	result := self.cachedSolutionsRef
 	self.cachedSolutionsLockRef.RUnlock()
 	return result
+
+}
+
+//The actual workhorse of solutions generating. 0 means "as many as you can
+//find". It might return more than you asked for, if it already had more
+//results than requested sitting around.
+func nOrFewerSolutions(grid Grid, max int) []Grid {
+	queueDone := make(chan bool, 1)
+
+	queue := newSyncedFiniteQueue(0, DIM*DIM, queueDone)
+
+	queue.In <- grid.Copy()
+
+	//In the past this wasn't buffered, but then when we finished early other items would try to go into it
+	//and block, which prevented them from looping back up and getting the signal to shut down.
+	//Since there's only a known number of threads, we'll make sure they all ahve a place to leave their work
+	//without blocking so they can get the signal to shut down.
+	incomingSolutions := make(chan Grid, _NUM_SOLVER_THREADS)
+
+	//Using a pattern for closing fan in style receivers from http://blog.golang.org/pipelines
+	var wg sync.WaitGroup
+
+	var solutions []Grid
+
+	//TODO: figure out a way to kill all of these threads when necessary.
+
+	//TODO: don't use a constant here, use someting around the lines of
+	//numCPU
+
+	for i := 0; i < _NUM_SOLVER_THREADS; i++ {
+		go func() {
+			//Sovler thread.
+			firstRun := true
+			defer wg.Done()
+			for {
+				grid, ok := <-queue.Out
+				if !ok {
+					return
+				}
+				result := searchGridSolutions(grid.(Grid), queue, firstRun, max)
+				if result != nil {
+					incomingSolutions <- result
+				}
+				queue.ItemDone <- true
+				firstRun = false
+			}
+		}()
+	}
+
+	wg.Add(_NUM_SOLVER_THREADS)
+
+	go func() {
+		wg.Wait()
+		close(incomingSolutions)
+	}()
+
+OuterLoop:
+	for {
+		select {
+		case solution := <-incomingSolutions:
+			//incomingSolutions must have been closed because no more work to do.
+			if solution == nil {
+				break OuterLoop
+			}
+			//Add it to results
+			solutions = append(solutions, solution)
+			if max > 0 && len(solutions) >= max {
+				break OuterLoop
+			}
+		case <-queueDone:
+			//Well, that's as good as it's going to get.
+			break OuterLoop
+		}
+	}
+
+	//In some cases that previous select would have something incoming on
+	//incomingsolutions, as well as on queueDone, and queueDone would have
+	//just so happened to have won. Check for one last remaining item
+	//coming in from incomingSolutions. Technically it's possible (how?)
+	//to have multiple items waiting on incomingSolutions, so read as many
+	//as we can get without blocking.(Not checking for this was the reason
+	//for bug #134.)
+
+DoneReading:
+	for {
+		select {
+		case solution := <-incomingSolutions:
+			if solution != nil {
+				solutions = append(solutions, solution)
+			}
+		default:
+			//Nope, guess there wasn't one left.
+			break DoneReading
+		}
+	}
+
+	//There might be some things waiting to go into incomingSolutions here, but because it has a slot
+	//for every thread to be buffered, it's OK, we can just stop now.
+
+	//TODO: the grids waiting in the queue will never have their .Done called. This isn't a big deal--GC should reclaim them--
+	//but we won't have as many that we could reuse.
+	queue.Exit <- true
+
+	return solutions
 
 }
 
