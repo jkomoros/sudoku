135a547f73d897d19231702aceb4cd8a3d6a2bb6
Alex Komoroske <jkomoros@gmail.com>
2016-08-26 17:29:02 -0700

    Added a TODO

--- grid.go
+++ grid.go
@@ -158,6 +158,9 @@ type Grid interface {
 
 //TODO: before pushing this, check performance delta. It's bad!
 
+//TODO: exhaustively test gridImpl and read only grids to make sure we're
+//actually exercising gridImpl, and not mutalbeGrids everywhere.
+
 //MutableGrid is a sudoku Grid that can be mutated.
 type MutableGrid interface {
 	//MutableGrid contains all of Grid's (read-only) methods.
