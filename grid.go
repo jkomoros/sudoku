4c825c0a8908d56127af32449a153aac5690549b
Alex Komoroske <jkomoros@gmail.com>
2016-10-02 07:46:01 -0400

    Added two TODOs about clean up before merging back to master.

--- grid.go
+++ grid.go
@@ -58,6 +58,12 @@ const (
 //things that take as input a MutableGrid or mutableGridImpl and then return a
 //Grid that is derived exactly form it.
 
+//TODO: update package comments about Grid/MutableGrid concepts before committing
+
+//TODO: go through and mark new TODOs with "." afterwards if they have been
+//affirmatively pushed back to "can do any time", so the "grep" for TODOs can
+//distinguish the ones I need to do before merging to master.
+
 //Grid is the primary type in the package. It represents a DIMxDIM sudoku puzzle that can
 //be acted on in various ways. Grid is read-only. For mutator methods, see MutableGrid.
 type Grid interface {
