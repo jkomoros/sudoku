10c58e8fc4151eeb0d86af5da42b02569f0dcf0a
Alex Komoroske <jkomoros@gmail.com>
2017-01-01 11:51:41 -0800

    TESTS FAIL. added debug function printFoundStepCacheItemChain

--- found_step_cache_test.go
+++ found_step_cache_test.go
@@ -1,6 +1,7 @@
 package sudoku
 
 import (
+	"log"
 	"testing"
 )
 
@@ -180,6 +181,17 @@ func TestFoundStepCacheGetSteps(t *testing.T) {
 		stepOne,
 	}, "after queue added first and second removed, first added again")
 
+}
+
+func printFoundStepCacheItemChain(item *foundStepCacheItem) {
+	i := 0
+	for item != nil {
+
+		log.Println("Item", i, &item, "prev:", item.prev, "next:", item.next, "step:", item.step)
+
+		item = item.next
+		i++
+	}
 }
 
 func getStepsHelper(t *testing.T, result []*SolveStep, golden []*SolveStep, description string) {
