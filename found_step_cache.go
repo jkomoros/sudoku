8965f599f73161e2d65d5b9f6946793488b4865d
Alex Komoroske <jkomoros@gmail.com>
2017-01-01 07:38:54 -0800

    A rough in of the implementation of found_steps_cache

---
+++ found_step_cache.go
@@ -0,0 +1,45 @@
+package sudoku
+
+//foundStepCache is a cache of steps that have already been found in a given
+//grid. It helps us reuse previously found steps to make repeated searches
+//fast.
+type foundStepCache struct {
+	//TODO: a field for which grid this is related to as a sanity check?
+
+	//TODO: consider caching the result to GetSteps and expiring if AddStep or
+	//RemoveStepsWithCells have been called.
+	firstItem *foundStepCacheItem
+	length    int
+}
+
+type foundStepCacheItem struct {
+	prev      *foundStepCacheItem
+	next      *foundStepCacheItem
+	foundStep *SolveStep
+}
+
+//AddStep adds a SolveStep to the cache.
+func (f *foundStepCache) AddStep(step *SolveStep) {
+
+	//TODO: implement
+
+	//TODO: how to handle adding steps that are effectively duplicates?
+}
+
+//RemoveStepsWithCells removes all steps whose target or pointer cells overlap
+//with cells. That is, steps who rely on something that has changed from when
+//they were earlier added.
+func (f *foundStepCache) RemoveStepsWithCells(cells []CellRef) {
+
+	//TODO: implement
+
+}
+
+//GetSteps gets all steps currently in the cache.
+func (f *foundStepCache) GetSteps() []*SolveStep {
+
+	//TODO: implement
+
+	return nil
+
+}
